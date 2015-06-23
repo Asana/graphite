7debe9aeb5f64977ddf3c2ff87a53b046e75cd51
Alvaro Morales <alvaro@asana.com>
2015-06-23 16:25:04 -0700

    Revert "Modified uwsgi recipe."
    This reverts commit 175161b90231519442562889e410d67642142f59.

--- attributes/uwsgi.rb
+++ attributes/uwsgi.rb
@@ -1,7 +1,6 @@
-default["graphite"]["uwsgi"]["socket"] = "#{default["graphite"]["base_dir"]/graphite.sock}"
-default["graphite"]["uwsgi"]["chmod_socket"] = 777
-default["graphite"]["uwsgi"]["home"] = default["graphite"]["base_dir"]
-default["graphite"]["uwsgi"]["master"] = true
-default["graphite"]["uwsgi"]["workers"] = 4
-default["graphite"]["uwsgi"]["plugins"] = ["python", "http"]
-default["graphite"]["uwsgi"]["wsgi_file"] = "#{default["graphite"]["base_dir"]}/conf/graphite.wsgi"
+default['graphite']['uwsgi']['socket'] = '/tmp/uwsgi.sock'
+default['graphite']['uwsgi']['workers'] = 8
+default['graphite']['uwsgi']['carbon'] = '127.0.0.1:2003'
+default['graphite']['uwsgi']['listen_http'] = false
+default['graphite']['uwsgi']['port'] = 8080
+default['graphite']['uwsgi']['service_type'] = 'runit'
